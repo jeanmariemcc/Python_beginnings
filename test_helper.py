4cb6c3fe6f44c1e8431e88403d93cd4ada482fc5
jeanmariemcc <84250957+jeanmariemcc@users.noreply.github.com>
2022-09-26 19:06:45 -0400

    Add files via upload

---
+++ test_helper.py
@@ -0,0 +1,6 @@
+def test_helper(actual, expected):
+    if actual == expected:
+        print(actual, "PASSED")
+    else:
+        print("ERROR: actual of:", actual, "!= to expected of:", expected
+
